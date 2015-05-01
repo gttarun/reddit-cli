4ff7d7ed35fb2f07f2800b87f327baee7058ade0
Tarun Gattu <gttarun@Taruns-MBP.attlocal.net>
2015-05-01 18:07:08 -0500

    data is parsed into a list of posts with title and links

--- xreddit.py
+++ xreddit.py
@@ -1,31 +1,32 @@
-#!/usr/bin/python
-
-# import sys
-
-# arguments = sys.argv
-# first_name  = sys.argv[1]
-# second_name = sys.argv[2]
-
-# print "Hello {0} {1} !!!!".format(first_name,second_name)
-
-
-# from PIL import Image
-# import urllib, cStringIO
-
-# file = cStringIO.StringIO(urllib.urlopen("http://hairstyles.thehairstyler.com/hairstyle_views/front_view_images/464/original/10551_Jared-Padalecki_copy_2.jpg").read())
-# img = Image.open(file)
-# img.show()
+# coding: utf-8
 
 import requests
 from bs4 import BeautifulSoup
 
-def pull_posts():
-	headers = {'User-Agent': 'reddit-cli'}
-	url = 'https://reddit.com/r/boxing'
+def get_posts():
 
-	response = requests.get(url, headers=headers)
-	soup = BeautifulSoup(response.text)
+    # set user agent and url to request response from reddit
+    headers = {'User-Agent': 'reddit-cli'}
+    url = 'https://reddit.com/r/boxing'
 
-	soup = soup.find(id='siteTable')
-	parsed = soup(attrs={'class': 'thing'})
-	parsed_more = [parsed(attrs={'class': 'title'}) for post in parsed]
+    # change html response into bs4 object and parse data 
+    response = requests.get(url, headers=headers)
+    html = response.text
+    soup = BeautifulSoup(html)
+    parsed = soup.find(id='siteTable')
+
+    # extract just the title of the post and its link
+    pre_posts = [post.find('p', attrs={'class' : 'title'}) for post in parsed(attrs={'class': 'thing'})]
+    posts = [[post.find('a').get('href'), post.get_text()] for post in pre_posts]
+
+    return posts # a list of posts with title and links
+ 
+def main():
+    posts = get_posts()
+
+    for post in posts:
+        print post, '\n'
+
+    return
+
+main()
