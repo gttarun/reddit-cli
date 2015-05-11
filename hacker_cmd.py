09f0b2d689845782cd7cced3e85b68f70e808982
Tarun Gattu <gttarun@yahoo.com>
2015-05-11 00:34:41 -0500

    added tag, for posts that have the flair attribute

--- hacker_cmd.py
+++ hacker_cmd.py
@@ -5,16 +5,6 @@ from PIL import Image
 import requests
 from StringIO import StringIO
 
-# NEED TO BE DONE
-
-# some pages have an extra post with no rank and others don't, sort accordingly
-# check {attr['class': 'domain']} for the redirect site (reddit, facebook, youtube, etc)
-# include number of upvotes
-# also need to probably  include a way to upvote as user, api call?
-# message system @user
-# different tabs in each subreddit (hot, new, rising, controversial, etc.)
-# clean up
-
 def get_posts(subreddit='', sort=''):
 
     # set user agent and url to request response from reddit
@@ -67,9 +57,17 @@ def get_posts(subreddit='', sort=''):
     for i in range(len(rank)):
         if rank[i] == '':
             rank[i] = '0'
-        posts[eval(rank[i])] = {'rank': eval(rank[i]), 'title':title[i], 'domain':domain[i], 
-                            'link':link[i], 'post_id':post_id[i], 'score':score[i],
-                            'comments':comments[i]}
+        posts[eval(rank[i])] = {'rank': eval(rank[i]), 'title':title[i], 'domain':domain[i],
+                                'link': link[i], 'post_id': post_id[i]}
+
+        try:
+            posts[eval(rank[i])]['score'] = score[i]
+        except:
+            posts[eval(rank[i])]['score'] = ''
+        try:
+            posts[eval(rank[i])]['comments'] = comments[i]
+        except:
+            posts[eval(rank[i])]['comments'] = ''
         try:
             posts[eval(rank[i])]['flair'] = flair[i][0]
         except:
@@ -209,8 +207,11 @@ class HackerCmd(cmd.Cmd):
                     response = requests.get(self.posts[rank]['link'])
                 else:
                     response = requests.get('http://i.' + self.posts[rank]['link'][7:] + ".jpg")
-                img = Image.open(StringIO(response.content))
-                img.show()
+                try:
+                    img = Image.open(StringIO(response.content))
+                    img.show()
+                except:
+                    webbrowser.open(self.posts[rank]['link'])
                 return
             webbrowser.open(self.posts[rank]['link'])
             return
